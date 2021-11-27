e03723cd6f3c57c15a43738c946ac922df4ca254
Wladislaw <65966048+Innaviton@users.noreply.github.com>
2021-11-28 00:00:30 +0300

    Доделал, первую страницу

--- darkgame/src/views/Home.vue
+++ darkgame/src/views/Home.vue
@@ -53,8 +53,8 @@
         <img id="console_room" src="../assets/console_room.png" />
         <div class="price_console"><span>149₽ час</span></div>
       </div>
-      <div class="description">
-        <!--<span id="name_room"> {{ name }}</span>-->
+    <!--  <div class="description">
+        <span id="name_room"> {{ name }}</span>
         <img
           id="rhomb_room"
           class="rhomb_room"
@@ -92,7 +92,7 @@
           src="../assets/rhomb_blue.svg"
         />
         <span id="text_room_6">{{ text_6 }}</span>
-      </div>
+      </div>-->
     </div>
     <div class="four_screen">
       <img src="../assets/four_screen.png" />
@@ -102,12 +102,32 @@
           <img id="img_news" src="../assets/img_news.png" />
           <div class="description">
             <span
-              >Проведение локального турнира между двумя лучшими команда...</span
+              >Проведение локального турнира между двумя лучшими
+              команда...</span
             >
           </div>
           <span id="date">24.08.2021</span>
         </div>
       </div>
+    </div>
+    <div class="footer">
+      <img src="../assets/footer.png" />
+      <div class="name_footer">
+        <span id="foot_1">DARKGAME</span><br /><span id="foot_2"
+          >ИГРОВОЙ КЛУБ</span
+        >
+      </div>
+      <span id="adress">г.Тула, ул.Республики д.49</span>
+      <img id="map" src="../assets/map.png" />
+      <div class="information">
+        <img id="vk" src="../assets/vk.svg" />
+        <a id="vk_span">darkgame_tula</a>
+        <img id="instagram" src="../assets/insta.svg" />
+        <a id="insta_span">darkgame_tula</a>
+        <span id="nomer_span">Контактный номер телефона</span>
+        <span id="tel">+7(929)123-21-32</span>
+      </div>
+      <span id="copirating">© 2024 DARKGAME</span>
     </div>
   </div>
 </template>
@@ -116,9 +136,9 @@
 export default {
   data() {
     return {
-      a: [1, 2, 3]
-    }
-  }
+      a: [1, 2, 3],
+    };
+  },
 };
 </script>
 
@@ -260,7 +280,7 @@ body {
   font-family: "Roboto" sans-serif;
   font-style: normal;
   font-weight: 900;
-  font-size: 54px;
+  font-size: 52px;
 
   text-align: center;
   color: #ffffff;
@@ -490,6 +510,7 @@ body {
   position: relative;
   width: 100%;
   background-color: #181823;
+  margin-top: -1%;
 }
 
 .four_screen > img {
@@ -545,24 +566,110 @@ body {
   color: #ffffff;
   word-wrap: break-word;
 }
-#date{
+#date {
   position: relative;
   width: 7.6%;
   font-size: 16px;
   color: #ffffff;
   left: 75%;
   top: 10%;
-
 }
 
-@media screen and (max-width: 1280px) {
-  #block_news{
+.footer {
+  position: relative;
+  width: 100%;
+}
+
+.name_footer {
+  position: absolute;
+  left: 3.5%;
+  top: 13%;
+  font-size: 24px;
+  font-family: "Roboto" sans-serif;
+  font-weight: bold;
+  line-height: 27px;
+}
+
+#foot_1 {
+  color: #ffffff;
+}
+#foot_2 {
+  font-size: 20px;
+  color: #7e7e7e;
+}
+
+#adress {
+  position: absolute;
+  width: 13%;
+  left: 3%;
+  font-family: "Roboto" sans-serif;
+  top: 65%;
+  color: #a4a4a4;
+}
+#map {
+  position: absolute;
+  top: 5%;
+  left: 20.5%;
+}
+#vk {
+  position: absolute;
+  top: 14%;
+  left: 68%;
+}
+#vk_span {
+  position: absolute;
+  width: 5.5%;
+  height: 1.5%;
+  top: 16%;
+  left: 71%;
+
+  color: #bbbbbb;
+}
+#instagram {
+  position: absolute;
+  top: 36.5%;
+  left: 68%;
+}
+#insta_span {
+  position: absolute;
+  width: 5.5%;
+  height: 1.5%;
+  top: 38.5%;
+  left: 71%;
+
+  color: #bbbbbb;
+}
+#nomer_span{
+  position: absolute;
+  color: #ffffff;
+  top: 19%;
+  left: 81%;
+}
+#tel{
+  position: absolute;
+  top: 32%;
+  left: 81%;
+  font-size: 28px;
+
+  color: #ffffff;
+}
+#copirating{
+  position: absolute;
+  right: 1%;
+  top: 85%;
+  color: #707070;
+}
+
+@media screen and (max-width: 1290px) {
+  #block_news {
     width: 20%;
   }
-  .description{
+  .description {
+    width: 100%;
     font-size: 12px;
   }
-  #date{
+  #date {
+    position: relative;
     top: 30%;
     font-size: 12px;
     left: 0%;
@@ -571,7 +678,7 @@ body {
     width: 290px;
     height: 159px;
   }
-  #name_news{
+  #name_news {
     font-size: 32px;
   }
   .slang {
@@ -627,6 +734,43 @@ body {
   .third_screen {
     margin-top: -10%;
   }
+  .name_footer{
+    font-size: 18px;
+  }
+  #adress{
+    font-size: 16p;
+  }
+  #map{
+    width: 40%;
+    top: 12.5%;
+    left: 22.5%;
+  }
+  #vk{
+    width: 20px;
+    height: 20px;
+  }
+  #instagram{
+    width: 20px;
+    height: 20px;
+  }
+  #vk_span{
+    font-size: 12px;
+  }
+  #insta_span{
+    font-size: 12px;
+  }
+  #nomer_span{
+    font-size: 13.8px;
+  }
+  #tel{
+    font-size: 18px;
+  }
+  #copirating{
+    font-size: 14px;
+  }
+  #foot_2{
+    font-size: 16px;
+  }
 }
 
 @media screen and (min-width: 901px) {
@@ -637,15 +781,15 @@ body {
     width: 170px;
     height: 90px;
   }
-  .description{
+  .description {
     font-size: 9.5px;
   }
-  #date{
+  #date {
     font-size: 9.5px;
     left: 0%;
-    top: 40%;
+    top: 25%;
   }
-   #name_news{
+  #name_news {
     font-size: 22px;
   }
   .slang {
@@ -706,39 +850,74 @@ body {
   #name_iron {
     font-size: 22px;
   }
-}
-
-@media screen and (min-width: 691px){ 
-}
-
-@media screen and (max-width: 690px){
-  #date{
-    top: 60%;
+  #vk{
+    width: 16px;
+    height: 16px;
   }
+  #vk_span{
+    font-size: 10px;
+  }
+  #instagram{
+    width: 16px;
+    height: 16px;
+    top: 30%;
+  }
+  #insta_span{
+    font-size: 10px;
+    top: 30%;
+  }
+  #nomer_span{
+    top: 50%;
+    left: 68%;
+    font-size: 12px;
+  }
+  #tel{
+    font-size: 12px;
+    top: 62%;
+    left: 68%;
+  }
+  .name_footer{
+    width: 18%;
+    word-wrap: break-word;
+    font-size: 16px;
+  }
+  #foot_2{
+    font-size: 12px;
+  }
+}
+
+@media screen and (min-width: 691px) {
+}
+
+@media screen and (max-width: 690px) {
+  #date {
+    top: 35%;
+  }
+  
 }
 @media screen and (min-width: 591px) {
 }
 
 @media screen and (max-width: 590px) {
-  #name_news{
+  #name_news {
     left: -10%;
   }
-  .blocks_news{
+  .blocks_news {
     width: 70%;
-  left: 10%;
+    left: 10%;
   }
-  #date{
+  #date {
     font-size: 8px;
     top: 70%;
   }
-  .four_screen{
+  .four_screen {
     width: 135%;
   }
-  .description{
+  .description {
     font-size: 8px;
     font-weight: normal;
   }
-  #block_news{
+  #block_news {
     left: -10%;
   }
   #logo {
@@ -746,7 +925,7 @@ body {
     height: 70px;
     top: 15%;
   }
-   #name_news{
+  #name_news {
     font-size: 12px;
   }
   .slang {
@@ -829,6 +1008,32 @@ body {
   .price_ordinary span {
     font-size: 9px;
   }
+  .name_footer{
+    font-size: 12px;
+  }
+  #foot_2{
+    font-size: 10px;
+  }
+  #adress{
+    width: 20%;
+    font-size: 12px;
+  }
+  #vk_span{
+    left: 73%;
+  }
+  #insta_span{
+    top: 32%;
+    left: 73%;
+  }
+  #nomer_span{
+    top: 55%;
+    left: 30%;
+  }
+  #tel{
+    top: 67%;
+    left: 41%;
+
+  }
 }
 
 @media screen and (min-width: 431px) {
@@ -888,6 +1093,17 @@ body {
   }
   .price_ordinary {
     left: 8%;
+  }
+  #name_footer{
+    font-size: 10px;
+  }
+  #foot_2{
+    font-size: 8px;
+  }
+  #adress{
+    top: 85%;
+    width: 90%;
+    font-size: 10px;
   }
 }
 </style>
